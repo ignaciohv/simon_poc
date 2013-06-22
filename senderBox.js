5bf0fa4a9db2b28f753d9e0c234bce2df2b93331
ignaciout <hermosillavenegas@gmail.com>
2013-06-22 09:40:55 -0400

    agregado archivo base

---
+++ senderBox.js
@@ -0,0 +1,10 @@
+var boxcar = require('boxcar');
+var provider = new boxcar.Provider('2KXkozIJdhwT9tyvukKf');
+var arguments = process.argv[2];
+//console.log(arguments);
+//send a message to a user directly
+
+setTimeout((function() {
+  provider.notify('mail.com',  process.argv[2]);
+ // provider.notify('magdabenavente@gmail.com',  process.argv[2]);
+}), 10000);
